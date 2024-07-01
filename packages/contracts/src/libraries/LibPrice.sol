a089e6da25df736bca6e74d7703dda5c922245fb
CometShock <cometshockcrypto@gmail.com>
2024-07-01 16:45:14 -0500

    chore: underscore prefix convention

--- packages/contracts/src/libraries/LibPrice.sol
+++ packages/contracts/src/libraries/LibPrice.sol
@@ -6,31 +6,31 @@ import { EEmpire, EAction } from "codegen/common.sol";
 import { EMPIRE_COUNT } from "src/constants.sol";
 
 library LibPrice {
-    function getTotalCost(EAction actionType, EEmpire empireImpacted, bool progressAction) internal view returns (uint256) {
+    function getTotalCost(EAction _actionType, EEmpire _empireImpacted, bool _progressAction) internal view returns (uint256) {
         uint256 totalCost = 0;
-        if (progressAction) {
-            require(actionType == EAction.CreateDestroyer, "[LibPrice] Action type is not a progressive action");
-            totalCost = getProgressPointCost(empireImpacted);
+        if (_progressAction) {
+            require(_actionType == EAction.CreateDestroyer, "[LibPrice] Action type is not a progressive action");
+            totalCost = getProgressPointCost(_empireImpacted);
         } else {
-            require(actionType == EAction.KillDestroyer, "[LibPrice] Action type is not a regressive action");
-            totalCost = getRegressPointCost(empireImpacted);
+            require(_actionType == EAction.KillDestroyer, "[LibPrice] Action type is not a regressive action");
+            totalCost = getRegressPointCost(_empireImpacted);
         }
 
-        totalCost += getActionMarginalCost(actionType, empireImpacted);
+        totalCost += getActionMarginalCost(_actionType, _empireImpacted);
         return totalCost;
     }
 
     // Gets the cost of the impacted empires points, iterated by EMPIRE_COUNT-1
-    function getProgressPointCost(EEmpire empireImpacted) internal view returns (uint256) {
-        return getPointCost(empireImpacted, EMPIRE_COUNT - 1);
+    function getProgressPointCost(EEmpire _empireImpacted) internal view returns (uint256) {
+        return getPointCost(_empireImpacted, EMPIRE_COUNT - 1);
     }
 
     // Gets the cost of a single point of each unimpacted empire
     // Iterates through each empire except the impacted one.
-    function getRegressPointCost(EEmpire empireImpacted) internal view returns (uint256) {
+    function getRegressPointCost(EEmpire _empireImpacted) internal view returns (uint256) {
         uint256 pointCost;
         for(uint256 i = 0; i < EMPIRE_COUNT; i++) {
-            if (i == uint256(empireImpacted)) {
+            if (i == uint256(_empireImpacted)) {
                 continue;
             }
             pointCost += getPointCost(EEmpire(i), 1);
@@ -39,20 +39,19 @@ library LibPrice {
     }
 
     // Gets the cost of a specific empire's points, for a given amount
-    function getPointCost(EEmpire empireImpacted, uint256 pointUnits) internal view returns (uint256) {
-        require(pointUnits > 0, "[LibPrice] Point units must be greater than 0");
-        uint256 initPointCost = Faction.getPointCost(empireImpacted);
+    function getPointCost(EEmpire _empireImpacted, uint256 _pointUnits) internal view returns (uint256) {
+        require(_pointUnits > 0, "[LibPrice] Point units must be greater than 0");
+        uint256 initPointCost = Faction.getPointCost(_empireImpacted);
         uint256 pointCostIncrease = P_GameConfig.getPointCostIncrease();
         uint256 pointCost = 0;
-        for(uint256 i = 0; i < pointUnits; i++) {
+        for(uint256 i = 0; i < _pointUnits; i++) {
             pointCost += initPointCost + i * pointCostIncrease;
         }
         return pointCost;
     }
 
     // Gets the cost of an action that impacts a specific empire
-    function getActionMarginalCost(EAction actionType, EEmpire empireImpacted) internal view returns (uint256) {
-        return Faction.getActionCost(empireImpacted)[uint256(actionType)];
+    function getActionMarginalCost(EAction _actionType, EEmpire _empireImpacted) internal view returns (uint256) {
+        return Faction.getActionCost(_empireImpacted)[uint256(_actionType)];
     }
-
 }
