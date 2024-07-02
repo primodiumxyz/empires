1d3c6d349eb5b8975b56ae5de19d949c536a8f6a
Nabs <43505437+NAB5@users.noreply.github.com>
2024-07-02 11:04:06 -0500

    build: get client to compile

--- apps/web/src/components/Game.tsx
+++ apps/web/src/components/Game.tsx
@@ -7,33 +7,15 @@ import { useTxExecute } from "@/hooks/useTxExecute";
 
 const Game = () => {
   const { tables } = useCore();
-  const count = tables.Counter.use()?.value;
   const doubleCount = tables.DoubleCounter.use()?.value;
   const { execute } = useTxExecute();
-
-  const handleIncrement = async () => {
-    await execute({
-      functionName: "Primodium_Base__increment",
-      args: [],
-      options: { gas: 100_000n },
-      txQueueOptions: {
-        id: defaultEntity,
-      },
-    });
-  };
 
   return (
     <div className="relative flex h-full w-full flex-col items-center justify-center gap-4">
       <Logout />
 
-      <h1 className="text-lg font-bold">Primodium Template</h1>
+      <h1 className="text-lg font-bold">Primodium Empires</h1>
       <img src={"primodium.jpg"} className="logo w-32" alt="Vite logo" />
-      <TransactionQueueMask id={defaultEntity} className="card">
-        <button className="btn btn-primary flex flex-col" onClick={handleIncrement}>
-          <p>count is {count ? formatNumber(count) : "N/A"}</p>
-          <p>doubled count is {doubleCount ? formatNumber(doubleCount) : "N/A"}</p>
-        </button>
-      </TransactionQueueMask>
     </div>
   );
 };
