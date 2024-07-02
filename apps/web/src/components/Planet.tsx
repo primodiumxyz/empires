ed33b29ea7b982e85474e6824cde625b66d793e9
Nabs <43505437+NAB5@users.noreply.github.com>
2024-07-02 18:46:04 -0500

    feat: show destroyer count

--- apps/web/src/components/Planet.tsx
+++ apps/web/src/components/Planet.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from "react";
+import { RocketLaunchIcon } from "@heroicons/react/24/solid";
 
 import { convertAxialToCartesian, entityToPlanetName } from "@primodiumxyz/core";
 import { useCore } from "@primodiumxyz/core/react";
@@ -35,10 +36,16 @@ export const Planet: React.FC<{ entity: Entity; tileSize: number; margin: number
         left: `${left}px`,
       }}
     >
-      <div className="mx-auto text-center font-bold text-white">
-        <p>{entityToPlanetName(entity)}</p>
-        <p>
-          ({Number(planet?.q ?? 0n)},{Number(planet?.r ?? 0n)})
+      <div className="flex flex-col items-center gap-2 text-white">
+        <div className="text-center">
+          <p className="font-bold">{entityToPlanetName(entity)}</p>
+          <p>
+            ({Number(planet.q ?? 0n)},{Number(planet.r ?? 0n)})
+          </p>
+        </div>
+
+        <p className="flex gap-2">
+          <RocketLaunchIcon className="size-6" /> {planet.destroyerCount.toLocaleString()}
         </p>
       </div>
     </Hexagon>
