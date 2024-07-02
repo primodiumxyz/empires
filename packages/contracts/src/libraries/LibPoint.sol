87aaddbfee915c0b7219e2ca34bdc1ae4cea2eb5
CometShock <cometshockcrypto@gmail.com>
2024-07-02 12:46:14 -0500

    chore: underscore prefix convention

--- packages/contracts/src/libraries/LibPoint.sol
+++ packages/contracts/src/libraries/LibPoint.sol
@@ -11,28 +11,28 @@ import { EEmpire } from "codegen/common.sol";
 library LibPoint {
   /**
    * @dev Issues points to a player for a specific empire. Does not manage prices.
-   * @param empire The empire to issue points for.
-   * @param playerId The ID of the player.
-   * @param points The number of points to issue.
+   * @param _empire The empire to issue points for.
+   * @param _playerId The ID of the player.
+   * @param _points The number of points to issue.
    */
-  function issuePoints(EEmpire empire, bytes32 playerId, uint256 points) internal {
-    require(empire != EEmpire.NULL && empire != EEmpire.LENGTH, "[LibPoint] Invalid empire");
-    Faction.setPointsIssued(empire, Faction.getPointsIssued(empire) + points);
-    Points.set(playerId, empire, Points.get(playerId, empire) + points);
+  function issuePoints(EEmpire _empire, bytes32 _playerId, uint256 _points) internal {
+    require(_empire != EEmpire.NULL && _empire != EEmpire.LENGTH, "[LibPoint] Invalid empire");
+    Faction.setPointsIssued(_empire, Faction.getPointsIssued(_empire) + _points);
+    Points.set(_playerId, _empire, Points.get(_playerId, _empire) + _points);
   }
 
   /**
    * @dev Removes points from a player for a specific empire. Does not manage prices.
-   * @param empire The empire to remove points from.
-   * @param playerId The ID of the player.
-   * @param points The number of points to remove.
+   * @param _empire The empire to remove points from.
+   * @param _playerId The ID of the player.
+   * @param _points The number of points to remove.
    */
-  function removePoints(EEmpire empire, bytes32 playerId, uint256 points) internal {
-    require(empire != EEmpire.NULL && empire != EEmpire.LENGTH, "[LibPoint] Invalid empire");
-    require(points <= Points.get(playerId, empire), "[LibPoint] Player does not have enough points to remove");
+  function removePoints(EEmpire _empire, bytes32 _playerId, uint256 _points) internal {
+    require(_empire != EEmpire.NULL && _empire != EEmpire.LENGTH, "[LibPoint] Invalid empire");
+    require(_points <= Points.get(_playerId, _empire), "[LibPoint] Player does not have enough points to remove");
     // Requires ordered in reverse of issuePoints() for clearer error message paths
-    require(points <= Faction.getPointsIssued(empire), "[LibPoint] Empire has not issued enough points to remove");
-    Points.set(playerId, empire, Points.get(playerId, empire) - points);
-    Faction.setPointsIssued(empire, Faction.getPointsIssued(empire) - points);
+    require(_points <= Faction.getPointsIssued(_empire), "[LibPoint] Empire has not issued enough points to remove");
+    Points.set(_playerId, _empire, Points.get(_playerId, _empire) - _points);
+    Faction.setPointsIssued(_empire, Faction.getPointsIssued(_empire) - _points);
   }
 }
