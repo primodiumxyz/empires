c71ac1afefa91e6bd9b39382c90bcbe5026e62a0
CometShock <cometshockcrypto@gmail.com>
2024-07-09 14:52:05 -0500

    fix: hank is a jokester

--- packages/contracts/src/libraries/InitPrice.sol
+++ packages/contracts/src/libraries/InitPrice.sol
@@ -8,10 +8,10 @@ import { EEmpire, EPlayerAction } from "codegen/common.sol";
 function initPrice() {
   uint256 startPointCost = P_PointConfig.getStartPointCost();
   uint256 startActionCost = P_ActionConfig.getStartActionCost();
-  for (uint256 q = 1; q < uint256(EEmpire.LENGTH); q++) {
-    Faction.setPointCost(EEmpire(q), startPointCost);
-    for (uint256 z = 1; z < uint256(EPlayerAction.LENGTH); z++) {
-      ActionCost.set(EEmpire(q), EPlayerAction(z), startActionCost);
+  for (uint256 i = 1; i < uint256(EEmpire.LENGTH); i++) {
+    Faction.setPointCost(EEmpire(i), startPointCost);
+    for (uint256 j = 1; j < uint256(EPlayerAction.LENGTH); j++) {
+      ActionCost.set(EEmpire(i), EPlayerAction(j), startActionCost);
     }
   }
 }
