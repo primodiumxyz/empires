ffe1de069e8b3c74340df8ea223e53d422ed7ac7
CometShock <cometshockcrypto@gmail.com>
2024-07-10 13:03:30 -0500

    feat: Reset PointsMap

--- packages/contracts/src/systems/ResetSystem.sol
+++ packages/contracts/src/systems/ResetSystem.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.8.24;
 import { System } from "@latticexyz/world/src/System.sol";
 import { PlanetsSet } from "adts/PlanetsSet.sol";
 import { FactionPlanetsSet } from "adts/FactionPlanetsSet.sol";
+import { PointsMap } from "adts/PointsMap.sol";
 import { EEmpire } from "codegen/common.sol";
 import { createPlanets } from "libraries/CreatePlanets.sol";
 import { initPrice } from "libraries/InitPrice.sol";
@@ -15,6 +16,12 @@ contract ResetSystem is System {
     FactionPlanetsSet.clear(EEmpire.Red);
     FactionPlanetsSet.clear(EEmpire.Blue);
     FactionPlanetsSet.clear(EEmpire.Green);
+    PointsMap.clear(EEmpire.Red);
+    PointsMap.clear(EEmpire.Blue);
+    PointsMap.clear(EEmpire.Green);
+    // Does not reset Player table, that's fine. it only contains id and spent
+    // by not clearing Player.spent, we can keep track of how much each player has spent over multiple matches
+
     RakeTaken.set(false);
     WinningEmpire.set(EEmpire.NULL);
 
