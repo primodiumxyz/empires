bd92606e84fe4b6968d834ed98ac883d9012cabd
CometShock <cometshockcrypto@gmail.com>
2024-07-10 11:31:44 -0500

    feat: more basic reset logic

--- packages/contracts/src/systems/ResetSystem.sol
+++ packages/contracts/src/systems/ResetSystem.sol
@@ -6,6 +6,8 @@ import { PlanetsSet } from "adts/PlanetsSet.sol";
 import { FactionPlanetsSet } from "adts/FactionPlanetsSet.sol";
 import { EEmpire } from "codegen/common.sol";
 import { createPlanets } from "libraries/CreatePlanets.sol";
+import { initPrice } from "libraries/InitPrice.sol";
+import { RakeTaken, WinningEmpire, P_GameConfig } from "codegen/index.sol";
 
 contract ResetSystem is System {
   function resetGame() public {
@@ -13,7 +15,11 @@ contract ResetSystem is System {
     FactionPlanetsSet.clear(EEmpire.Red);
     FactionPlanetsSet.clear(EEmpire.Blue);
     FactionPlanetsSet.clear(EEmpire.Green);
+    RakeTaken.set(false);
+    WinningEmpire.set(EEmpire.NULL);
 
-    createPlanets();
+    P_GameConfig.setGameOverBlock(block.number + 1_000);
+    createPlanets(); // Planet and Faction tables are reset to default values
+    initPrice(); // Faction.setPointCost and ActionCost tables are reset to default values
   }
 }
