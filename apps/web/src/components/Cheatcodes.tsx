e8beedfa201b90dcebbee771bee59ac07553c428
polarzero <0xpolarzero@gmail.com>
2024-07-10 18:01:48 +0200

    refactor: add select option & fixes

--- apps/web/src/components/Cheatcodes.tsx
+++ apps/web/src/components/Cheatcodes.tsx
@@ -3,13 +3,11 @@ import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 import { useAccountClient, useCore } from "@primodiumxyz/core/react";
 import CheatcodesButton, { CheatcodesCloseButton } from "@/components/CheatcodesButton";
 import { setupCheatcodes } from "@/config/setupCheatcodes";
-import { CheatcodeInputs, CheatcodeInputsBase, Cheatcode as CheatcodeType, createCheatcode } from "@/util/cheatcodes";
+import { useContractCalls } from "@/hooks/useContractCalls";
+import { CheatcodeInputs, CheatcodeInputsBase, Cheatcode as CheatcodeType, formatValue } from "@/util/cheatcodes";
 import { cn } from "@/util/client";
 
 import "@/index.css";
-
-import { useContractCalls } from "@/hooks/useContractCalls";
-import { useTxExecute } from "@/hooks/useTxExecute";
 
 /* -------------------------------------------------------------------------- */
 /*                                 CHEATCODES                                 */
@@ -122,20 +120,44 @@ const Cheatcode = <T extends CheatcodeInputsBase>({
         )}
       >
         {Object.entries(inputs).map(([inputKey, input]) => {
+          const { label, inputType = "string", options = [] } = input;
+          // default value will be either provided, or first option if any, or default value corresponding to the input type
+          const defaultValue = formatValue(inputType, input.defaultValue ?? options[0]).toString();
+
           return (
             <div key={inputKey} className="flex flex-col gap-1 text-sm">
-              <label className="text-gray-300">{input.label}</label>
-              <input
-                type={input.inputType || "text"}
-                placeholder={input.defaultValue.toString()}
-                onChange={(e) => {
-                  setInputValues((prev) => ({
-                    ...prev,
-                    [inputKey]: { ...input, value: e.target.value },
-                  }));
-                }}
-                className="max-h-8 bg-gray-800 p-2 text-gray-300"
-              />
+              <label className="text-gray-300">{label}</label>
+              {options.length > 0 ? (
+                <select
+                  defaultValue={defaultValue}
+                  onChange={(e) => {
+                    setInputValues((prev) => ({
+                      ...prev,
+                      [inputKey]: { ...input, value: formatValue(inputType, e.target.value) },
+                    }));
+                  }}
+                  className="max-h-8 bg-gray-800 p-2 text-gray-300"
+                >
+                  {options?.map((option, i) => (
+                    <option key={i} value={option.toString()}>
+                      {option.toString()}
+                    </option>
+                  ))}
+                </select>
+              ) : (
+                <input
+                  type="text"
+                  placeholder={defaultValue}
+                  defaultValue={defaultValue}
+                  onChange={(e) => {
+                    setInputValues((prev) => ({
+                      ...prev,
+                      [inputKey]: { ...input, value: formatValue(inputType, e.target.value) },
+                    }));
+                  }}
+                  className="max-h-8 bg-gray-800 p-2 text-gray-300"
+                />
+              )}
             </div>
           );
         })}
