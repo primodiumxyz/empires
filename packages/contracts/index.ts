c5f0082ecc1f15485e1720e8fdf87567b14a383f
polarzero <0xpolarzero@gmail.com>
2024-07-11 13:23:32 +0200

    refactor: export constants

--- packages/contracts/index.ts
+++ packages/contracts/index.ts
@@ -2,5 +2,17 @@ import mudConfig, { worldInput } from "./mud.config";
 import IWorldAbi from "./out/IWorld.sol/IWorld.abi.json";
 import worldsJson from "./worlds.json";
 import { EDirection, EEmpire, EMovement, EOrigin } from "./config/enums";
+import { POINTS_UNIT, OTHER_EMPIRE_COUNT } from "./config/constants";
 
-export { mudConfig, IWorldAbi, worldsJson, worldInput, EDirection, EEmpire, EMovement, EOrigin };
+export {
+  mudConfig,
+  IWorldAbi,
+  worldsJson,
+  worldInput,
+  EDirection,
+  EEmpire,
+  EMovement,
+  EOrigin,
+  POINTS_UNIT,
+  OTHER_EMPIRE_COUNT,
+};
