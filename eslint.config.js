1dc579c903a1b716c69421c43e33b9f26f3f5796
polarzero <0xpolarzero@gmail.com>
2024-07-11 02:27:17 +0200

    fix: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,10 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked, {
+  ignores: ["**/dist/**", "**/build/**", "**/vite.config.ts"],
   languageOptions: {
     parserOptions: {
       project: ["./packages/*/tsconfig.json", "./apps/*/tsconfig.json"],
     },
   },
-  ignores: ["vite.config.ts"],
 });
