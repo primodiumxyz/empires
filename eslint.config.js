0054044b7a81a20eeffeaab8c92c4fdcec25d837
polarzero <0xpolarzero@gmail.com>
2024-07-11 02:25:55 +0200

    fix: parser options

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked, {
   languageOptions: {
     parserOptions: {
-      project: ["./packages/*/tsconfig.json"],
+      project: ["./packages/*/tsconfig.json", "./apps/*/tsconfig.json"],
     },
   },
   ignores: ["vite.config.ts"],
