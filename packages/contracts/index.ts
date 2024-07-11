49dad49057b381b528f47e652adb0dd3ff4fd660
polarzero <0xpolarzero@gmail.com>
2024-07-11 15:59:49 +0200

    fix: add contract import

--- packages/contracts/index.ts
+++ packages/contracts/index.ts
@@ -1,7 +1,7 @@
 import mudConfig, { worldInput } from "./mud.config";
 import IWorldAbi from "./out/IWorld.sol/IWorld.abi.json";
 import worldsJson from "./worlds.json";
-import { EDirection, EEmpire, EMovement, EOrigin } from "./config/enums";
+import { EDirection, EEmpire, EMovement, ENPCAction, EOrigin } from "./config/enums";
 import { POINTS_UNIT, OTHER_EMPIRE_COUNT } from "./config/constants";
 
 export {
@@ -12,6 +12,7 @@ export {
   EDirection,
   EEmpire,
   EMovement,
+  ENPCAction,
   EOrigin,
   POINTS_UNIT,
   OTHER_EMPIRE_COUNT,
