b25e0f8cb4d68c07c699e332a21dbb82b801a472
0xhank <zeroxhank@gmail.com>
2024-07-15 13:49:44 -0400

    feat: show conversion in usd

--- apps/web/src/components/ActionLog.tsx
+++ apps/web/src/components/ActionLog.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
-import { formatEther } from "viem";
+import { useMemo } from "react";
 
 import { entityToPlanetName, formatAddress } from "@primodiumxyz/core";
 import { useCore } from "@primodiumxyz/core/react";
 import { Entity } from "@primodiumxyz/reactive-tables";
+import { useEthPrice } from "@/hooks/useEthPrice";
 
 type ActionLogEntry = {
   actor: string;
@@ -13,24 +13,23 @@ type ActionLogEntry = {
 };
 
 export const ActionLog = () => {
-  const { tables } = useCore();
+  const { tables, utils } = useCore();
 
   const moveActions = tables.MoveNPCAction.useAll();
   const battleActions = tables.BattleNPCAction.useAll();
   const buyActions = tables.BuyDestroyersNPCAction.useAll();
   const createActions = tables.CreateDestroyerPlayerAction.useAll();
   const killActions = tables.KillDestroyerPlayerAction.useAll();
+  const { price } = useEthPrice();
 
-  const [actions, setActions] = useState<ActionLogEntry[]>([]);
-
-  useEffect(() => {
+  const actions = useMemo(() => {
     const moveActionEntries = moveActions.map((actionEntity) => {
       const action = tables.MoveNPCAction.get(actionEntity)!;
       return {
         actor: entityToPlanetName(action.originPlanetId as Entity),
         type: "Move",
         timestamp: action.timestamp,
-        details: `Moved ${action.shipCount} ships from ${entityToPlanetName(action.originPlanetId as Entity)} to ${entityToPlanetName(action.destinationPlanetId as Entity)}`,
+        details: `Ships: ${action.shipCount}, From: ${entityToPlanetName(action.originPlanetId as Entity)}, To: ${entityToPlanetName(action.destinationPlanetId as Entity)}`,
       };
     });
 
@@ -40,7 +39,7 @@ export const ActionLog = () => {
         actor: entityToPlanetName(action.planetId as Entity),
         type: "Battle",
         timestamp: action.timestamp,
-        details: `Battle at ${entityToPlanetName(action.planetId as Entity)}: ${action.attackingShipCount} vs ${action.defendingShipCount}. Conquered: ${action.conquer}`,
+        details: `At: ${entityToPlanetName(action.planetId as Entity)}, Attacking: ${action.attackingShipCount}, Defending: ${action.defendingShipCount}`,
       };
     });
 
@@ -50,7 +49,7 @@ export const ActionLog = () => {
         actor: entityToPlanetName(action.planetId as Entity),
         type: "Buy",
         timestamp: action.timestamp,
-        details: `Bought ${action.destroyerBought} destroyers for ${formatEther(action.goldSpent)} gold at ${entityToPlanetName(action.planetId as Entity)}`,
+        details: `Bought: ${action.destroyerBought}, Gold: ${action.goldSpent}, At: ${entityToPlanetName(action.planetId as Entity)}`,
       };
     });
 
@@ -60,7 +59,7 @@ export const ActionLog = () => {
         actor: formatAddress(action.playerId, true),
         type: "Create Ships",
         timestamp: action.timestamp,
-        details: `At ${entityToPlanetName(action.planetId as Entity)}. Ship count: ${action.newDestroyerCount}. Spent: ${formatEther(action.ethSpent)}ETH`,
+        details: `At: ${entityToPlanetName(action.planetId as Entity)}, Ship count: ${action.newDestroyerCount}, Spent: ${utils.ethToUSD(action.ethSpent, price ?? 0)}`,
       };
     });
 
@@ -70,7 +69,7 @@ export const ActionLog = () => {
         actor: formatAddress(action.playerId, true),
         type: "Kill Ships",
         timestamp: action.timestamp,
-        details: `At ${entityToPlanetName(action.planetId as Entity)}. Ship count: ${action.newDestroyerCount}. Spent: ${formatEther(action.ethSpent)}ETH`,
+        details: `At: ${entityToPlanetName(action.planetId as Entity)}, Ship count: ${action.newDestroyerCount}, Spent: ${utils.ethToUSD(action.ethSpent, price ?? 0)}`,
       };
     });
 
@@ -82,11 +81,11 @@ export const ActionLog = () => {
       ...killActionEntries,
     ];
     allActions.sort((a, b) => Number(b.timestamp - a.timestamp));
-    setActions(allActions);
+    return allActions;
   }, [moveActions, battleActions, buyActions, createActions, killActions]);
 
   return (
-    <div className="absolute bottom-4 left-4 flex max-h-96 w-72 flex-col gap-2 overflow-y-auto rounded bg-secondary p-4 text-white">
+    <div className="absolute bottom-4 left-4 flex h-3/4 w-72 flex-col gap-2 overflow-y-auto rounded bg-secondary p-4 text-white">
       <h2 className="mb-2 text-xs font-bold uppercase">Action Log</h2>
       {actions.map((action, index) => (
         <div key={index} className="rounded border border-white/20 bg-white/20 p-1 text-sm">
@@ -94,7 +93,7 @@ export const ActionLog = () => {
             {action.type} ({action.actor})
           </p>
           <p className="text-[0.7rem]">{action.details}</p>
-          <p className="text-xs text-gray-400">{new Date(Number(action.timestamp) * 1000).toLocaleString()}</p>
+          <p className="text-xs text-gray-300">{new Date(Number(action.timestamp) * 1000).toLocaleString()}</p>
         </div>
       ))}
     </div>
