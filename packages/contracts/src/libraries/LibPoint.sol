a45ef33558bc87c749d89afc59c3666cbfe2530a
CometShock <cometshockcrypto@gmail.com>
2024-07-16 15:12:49 -0500

    refactor: _points to _pointsScaled

--- packages/contracts/src/libraries/LibPoint.sol
+++ packages/contracts/src/libraries/LibPoint.sol
@@ -14,23 +14,23 @@ library LibPoint {
    * @dev Issues points to a player for a specific empire. Does not manage prices.
    * @param _empire The empire to issue points for.
    * @param _playerId The ID of the player.
-   * @param _points The number of points to issue.
+   * @param _pointsScaled The number of points to issue. (scaled by 1e18)
    */
-  function issuePoints(EEmpire _empire, bytes32 _playerId, uint256 _points) internal {
+  function issuePoints(EEmpire _empire, bytes32 _playerId, uint256 _pointsScaled) internal {
     require(_empire != EEmpire.NULL && _empire != EEmpire.LENGTH, "[LibPoint] Invalid empire");
-    PointsMap.set(_empire, _playerId, PointsMap.get(_empire, _playerId) + _points);
+    PointsMap.set(_empire, _playerId, PointsMap.get(_empire, _playerId) + _pointsScaled);
   }
 
   /**
    * @dev Removes points from a player for a specific empire. Does not manage prices.
    * @param _empire The empire to remove points from.
    * @param _playerId The ID of the player.
-   * @param _points The number of points to remove.
+   * @param _pointsScaled The number of points to remove. (scaled by 1e18)
    */
-  function removePoints(EEmpire _empire, bytes32 _playerId, uint256 _points) internal {
-    require(_points <= PointsMap.get(_empire, _playerId), "[LibPoint] Player does not have enough points to remove");
+  function removePoints(EEmpire _empire, bytes32 _playerId, uint256 _pointsScaled) internal {
+    require(_pointsScaled <= PointsMap.get(_empire, _playerId), "[LibPoint] Player does not have enough points to remove");
     // Requires ordered in reverse of issuePoints() for clearer error message paths
-    require(_points <= Faction.getPointsIssued(_empire), "[LibPoint] Empire has not issued enough points to remove");
-    PointsMap.set(_empire, _playerId, PointsMap.get(_empire, _playerId) - _points);
+    require(_pointsScaled <= Faction.getPointsIssued(_empire), "[LibPoint] Empire has not issued enough points to remove");
+    PointsMap.set(_empire, _playerId, PointsMap.get(_empire, _playerId) - _pointsScaled);
   }
 }
