434e29b8c2df550b3fca853bc78f8c6f7436d804
kethcode <kethic@kethic.com>
2024-07-17 15:55:56 -0700

    test: added tests, fixed bugs.

--- packages/contracts/test/systems/ActionSystem.t.sol
+++ packages/contracts/test/systems/ActionSystem.t.sol
@@ -1,116 +1,143 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.24;
 
-import { console, PrimodiumTest } from "test/PrimodiumTest.t.sol";
-import { Planet, ActionCost, Player, P_PointConfig } from "codegen/index.sol";
-import { Balances } from "@latticexyz/world/src/codegen/tables/Balances.sol";
-import { PointsMap } from "adts/PointsMap.sol";
-import { PlanetsSet } from "adts/PlanetsSet.sol";
-import { LibPrice } from "libraries/LibPrice.sol";
-import { EEmpire, EPlayerAction } from "codegen/common.sol";
-import { addressToId } from "src/utils.sol";
-import { EMPIRES_NAMESPACE_ID, EMPIRE_COUNT } from "src/constants.sol";
+import {console, PrimodiumTest} from "test/PrimodiumTest.t.sol";
+import {Planet, ActionCost, Player, P_PointConfig} from "codegen/index.sol";
+import {Balances} from "@latticexyz/world/src/codegen/tables/Balances.sol";
+import {PointsMap} from "adts/PointsMap.sol";
+import {PlanetsSet} from "adts/PlanetsSet.sol";
+import {LibPrice} from "libraries/LibPrice.sol";
+import {EEmpire, EPlayerAction} from "codegen/common.sol";
+import {addressToId} from "src/utils.sol";
+import {EMPIRES_NAMESPACE_ID, EMPIRE_COUNT} from "src/constants.sol";
 
 contract ActionSystemTest is PrimodiumTest {
-  bytes32 planetId;
-  bytes32 aliceId;
-  bytes32 bobId;
+    bytes32 planetId;
+    bytes32 aliceId;
+    bytes32 bobId;
 
-  function setUp() public override {
-    super.setUp();
-    uint256 i = 0;
-    do {
-      planetId = PlanetsSet.getPlanetIds()[i];
-      i++;
-    } while (Planet.getFactionId(planetId) == EEmpire.NULL);
-    aliceId = addressToId(alice);
-    bobId = addressToId(bob);
-    vm.prank(creator);
-    P_PointConfig.setPointRake(0);
-  }
+    function setUp() public override {
+        super.setUp();
+        uint256 i = 0;
+        do {
+            planetId = PlanetsSet.getPlanetIds()[i];
+            i++;
+        } while (Planet.getFactionId(planetId) == EEmpire.NULL);
+        aliceId = addressToId(alice);
+        bobId = addressToId(bob);
+        vm.prank(creator);
+        P_PointConfig.setPointRake(0);
+    }
 
-  function testCreateDestroyer() public {
-    uint256 cost = LibPrice.getTotalCost(EPlayerAction.CreateDestroyer, Planet.getFactionId(planetId), true);
-    world.Empires__createDestroyer{ value: cost }(planetId);
-    assertEq(Planet.get(planetId).destroyerCount, 1);
-  }
+    function testCreateDestroyer() public {
+        uint256 cost = LibPrice.getTotalCost(EPlayerAction.CreateDestroyer, Planet.getFactionId(planetId), true);
+        world.Empires__createDestroyer{value: cost}(planetId);
+        assertEq(Planet.get(planetId).destroyerCount, 1);
+    }
 
-  function testKillDestroyer() public {
-    uint256 cost = LibPrice.getTotalCost(EPlayerAction.CreateDestroyer, Planet.getFactionId(planetId), true);
-    world.Empires__createDestroyer{ value: cost }(planetId);
-    assertEq(Planet.get(planetId).destroyerCount, 1);
+    function testKillDestroyer() public {
+        uint256 cost = LibPrice.getTotalCost(EPlayerAction.CreateDestroyer, Planet.getFactionId(planetId), true);
+        world.Empires__createDestroyer{value: cost}(planetId);
+        assertEq(Planet.get(planetId).destroyerCount, 1);
 
-    cost = LibPrice.getTotalCost(EPlayerAction.KillDestroyer, Planet.getFactionId(planetId), false);
-    world.Empires__killDestroyer{ value: cost }(planetId);
-    assertEq(Planet.get(planetId).destroyerCount, 0);
-  }
+        cost = LibPrice.getTotalCost(EPlayerAction.KillDestroyer, Planet.getFactionId(planetId), false);
+        world.Empires__killDestroyer{value: cost}(planetId);
+        assertEq(Planet.get(planetId).destroyerCount, 0);
+    }
 
-  function testKillDestroyerFailNoDestroyers() public {
-    vm.expectRevert("[ActionSystem] No destroyers to kill");
-    world.Empires__killDestroyer(planetId);
-  }
+    function testChargeShield() public {
+        uint256 currentShields = Planet.get(planetId).shieldCount;
+        uint256 cost = LibPrice.getTotalCost(EPlayerAction.ChargeShield, Planet.getFactionId(planetId), true);
+        world.Empires__chargeShield{value: cost}(planetId);
+        assertEq(Planet.get(planetId).shieldCount, currentShields + 1);
+    }
 
-  function testCreateFailNotOwned() public {
-    bytes32 nonOwnedPlanetId;
-    uint256 i = 0;
-    do {
-      nonOwnedPlanetId = PlanetsSet.getPlanetIds()[i];
-      i++;
-    } while (Planet.getFactionId(nonOwnedPlanetId) != EEmpire.NULL);
+    function testDrainShield() public {
+        uint256 cost = 0;
+        uint256 currentShields = Planet.get(planetId).shieldCount;
+        if (currentShields == 0) {
+            cost = LibPrice.getTotalCost(EPlayerAction.ChargeShield, Planet.getFactionId(planetId), true);
+            world.Empires__chargeShield{value: cost}(planetId);
+            currentShields = 1;
+            assertEq(Planet.get(planetId).shieldCount, currentShields);
+        }
 
-    vm.expectRevert("[ActionSystem] Planet is not owned");
-    world.Empires__createDestroyer(nonOwnedPlanetId);
-  }
+        cost = LibPrice.getTotalCost(EPlayerAction.DrainShield, Planet.getFactionId(planetId), false);
+        world.Empires__drainShield{value: cost}(planetId);
+        assertEq(Planet.get(planetId).shieldCount, currentShields - 1);
+    }
 
-  function testPurchaseActionProgress() public {
-    EEmpire empire = Planet.getFactionId(planetId);
-    uint256 totalCost = LibPrice.getTotalCost(EPlayerAction.CreateDestroyer, empire, true);
-    uint256 actionCost = ActionCost.get(empire, EPlayerAction.CreateDestroyer);
+    function testKillDestroyerFailNoDestroyers() public {
+        vm.expectRevert("[ActionSystem] No destroyers to kill");
+        world.Empires__killDestroyer(planetId);
+    }
 
-    vm.startPrank(alice);
-    world.Empires__createDestroyer{ value: totalCost }(planetId);
-    assertGt(
-      LibPrice.getTotalCost(EPlayerAction.CreateDestroyer, empire, true),
-      totalCost,
-      "Total Cost should have increased"
-    );
-    assertGt(ActionCost.get(empire, EPlayerAction.CreateDestroyer), actionCost, "Action Cost should have increased");
-    assertEq(Player.getSpent(aliceId), totalCost, "Player should have spent total cost");
-    assertEq(Balances.get(EMPIRES_NAMESPACE_ID), totalCost, "Namespace should have received the balance");
-    assertEq(
-      PointsMap.get(EEmpire.Red, aliceId),
-      (EMPIRE_COUNT - 1) * P_PointConfig.getPointUnit(),
-      "Player should have received points"
-    );
-  }
+    function testDrainShieldFailNoShield() public {
+        vm.expectRevert("[ActionSystem] No shields to drain");
+        world.Empires__drainShield(planetId);
+    }
 
-  function testPurchaseActionRegress() public {
-    testPurchaseActionProgress();
+    function testCreateFailNotOwned() public {
+        bytes32 nonOwnedPlanetId;
+        uint256 i = 0;
+        do {
+            nonOwnedPlanetId = PlanetsSet.getPlanetIds()[i];
+            i++;
+        } while (Planet.getFactionId(nonOwnedPlanetId) != EEmpire.NULL);
 
-    EEmpire empire = Planet.getFactionId(planetId);
-    uint256 totalCost = LibPrice.getTotalCost(EPlayerAction.KillDestroyer, empire, false);
-    uint256 actionCost = ActionCost.get(empire, EPlayerAction.KillDestroyer);
-    uint256 initBalance = Balances.get(EMPIRES_NAMESPACE_ID);
+        vm.expectRevert("[ActionSystem] Planet is not owned");
+        world.Empires__createDestroyer(nonOwnedPlanetId);
+    }
 
-    vm.startPrank(bob);
-    world.Empires__killDestroyer{ value: totalCost }(planetId);
-    assertGt(
-      LibPrice.getTotalCost(EPlayerAction.KillDestroyer, empire, false),
-      totalCost,
-      "Total Cost should have increased"
-    );
-    assertGt(ActionCost.get(empire, EPlayerAction.KillDestroyer), actionCost, "Action Cost should have increased");
-    assertEq(Player.getSpent(bobId), totalCost, "Player should have spent total cost");
-    assertEq(Balances.get(EMPIRES_NAMESPACE_ID), initBalance + totalCost, "Namespace should have received the balance");
-    assertEq(
-      PointsMap.get(EEmpire.Blue, bobId),
-      P_PointConfig.getPointUnit(),
-      "Player should have received blue points"
-    );
-    assertEq(
-      PointsMap.get(EEmpire.Green, bobId),
-      P_PointConfig.getPointUnit(),
-      "Player should have received green points"
-    );
-  }
+    function testPurchaseActionProgress() public {
+        EEmpire empire = Planet.getFactionId(planetId);
+        uint256 totalCost = LibPrice.getTotalCost(EPlayerAction.CreateDestroyer, empire, true);
+        uint256 actionCost = ActionCost.get(empire, EPlayerAction.CreateDestroyer);
+
+        vm.startPrank(alice);
+        world.Empires__createDestroyer{value: totalCost}(planetId);
+        assertGt(
+            LibPrice.getTotalCost(EPlayerAction.CreateDestroyer, empire, true),
+            totalCost,
+            "Total Cost should have increased"
+        );
+        assertGt(ActionCost.get(empire, EPlayerAction.CreateDestroyer), actionCost, "Action Cost should have increased");
+        assertEq(Player.getSpent(aliceId), totalCost, "Player should have spent total cost");
+        assertEq(Balances.get(EMPIRES_NAMESPACE_ID), totalCost, "Namespace should have received the balance");
+        assertEq(
+            PointsMap.get(EEmpire.Red, aliceId),
+            (EMPIRE_COUNT - 1) * P_PointConfig.getPointUnit(),
+            "Player should have received points"
+        );
+    }
+
+    function testPurchaseActionRegress() public {
+        testPurchaseActionProgress();
+
+        EEmpire empire = Planet.getFactionId(planetId);
+        uint256 totalCost = LibPrice.getTotalCost(EPlayerAction.KillDestroyer, empire, false);
+        uint256 actionCost = ActionCost.get(empire, EPlayerAction.KillDestroyer);
+        uint256 initBalance = Balances.get(EMPIRES_NAMESPACE_ID);
+
+        vm.startPrank(bob);
+        world.Empires__killDestroyer{value: totalCost}(planetId);
+        assertGt(
+            LibPrice.getTotalCost(EPlayerAction.KillDestroyer, empire, false),
+            totalCost,
+            "Total Cost should have increased"
+        );
+        assertGt(ActionCost.get(empire, EPlayerAction.KillDestroyer), actionCost, "Action Cost should have increased");
+        assertEq(Player.getSpent(bobId), totalCost, "Player should have spent total cost");
+        assertEq(
+            Balances.get(EMPIRES_NAMESPACE_ID), initBalance + totalCost, "Namespace should have received the balance"
+        );
+        assertEq(
+            PointsMap.get(EEmpire.Blue, bobId), P_PointConfig.getPointUnit(), "Player should have received blue points"
+        );
+        assertEq(
+            PointsMap.get(EEmpire.Green, bobId),
+            P_PointConfig.getPointUnit(),
+            "Player should have received green points"
+        );
+    }
 }
