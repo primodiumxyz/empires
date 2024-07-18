e8dd65697ebb9d2158db564675aeb9d3aa75253c
polarzero <0xpolarzero@gmail.com>
2024-07-18 12:21:33 +0200

    feat: mock faction sprites

---
+++ packages/assets/mappings/sprite.ts
@@ -0,0 +1,17 @@
+// TODO: Temporary files for rendering in the client
+import FactionRedImg from '../sprites/factions/faction_red.png';
+import FactionBlueImg from '../sprites/factions/faction_blue.png';
+import FactionGreenImg from '../sprites/factions/faction_green.png';
+
+// export enum Sprites {
+//   FactionRed = 'sprites/factions/faction_red.png',
+//   FactionBlue = 'sprites/factions/faction_blue.png',
+//   FactionGreen = 'sprites/factions/faction_green.png',
+// }
+
+// TODO: Temporary fake sprites for rendering in the client
+export const Sprites = {
+  FactionRed: FactionRedImg,
+  FactionBlue: FactionBlueImg,
+  FactionGreen: FactionGreenImg,
+};
---
+++ packages/game/src/lib/mappings.ts
@@ -0,0 +1,9 @@
+import { EEmpire } from '@primodiumxyz/contracts';
+import { Sprites } from '@primodiumxyz/assets';
+
+export const EmpireToFactionSprites = {
+  [EEmpire.Red]: Sprites.FactionRed,
+  [EEmpire.Green]: Sprites.FactionGreen,
+  [EEmpire.Blue]: Sprites.FactionBlue,
+  [EEmpire.LENGTH]: undefined,
+};
