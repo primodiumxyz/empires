f5d5b42fff2fd52b9ec9b05010e8590f2ffca9a0
polarzero <0xpolarzero@gmail.com>
2024-07-30 19:09:18 +0200

    fix: lookup revert

--- packages/core/src/lib/lookups.ts
+++ packages/core/src/lib/lookups.ts
@@ -1,4 +1,4 @@
 export const RevertMessageToUserError = {
-  "Execution reverted with reason: revert: [ActionSystem] Incorrect payment.":
+  "Execution reverted with reason: revert: [OverrideSystem] Incorrect payment.":
     "Price for this action has changed. Please try again.",
 };
