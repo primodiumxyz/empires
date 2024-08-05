af6718178e9db61c08a27a2a023fbeef54c0f2c7
polarzero <0xpolarzero@gmail.com>
2024-08-05 11:14:37 +0200

    fix: reset turn & set next turn block in PostDeploy

--- packages/contracts/src/systems/ResetSystem.sol
+++ packages/contracts/src/systems/ResetSystem.sol
@@ -8,7 +8,7 @@ import { PointsMap } from "adts/PointsMap.sol";
 import { EEmpire } from "codegen/common.sol";
 import { createPlanets } from "codegen/scripts/CreatePlanets.sol";
 import { initPrice } from "libraries/InitPrice.sol";
-import { PendingMove, WinningEmpire, HistoricalPointCost, P_GameConfig } from "codegen/index.sol";
+import { PendingMove, WinningEmpire, HistoricalPointCost, Turn, P_GameConfig } from "codegen/index.sol";
 
 contract ResetSystem is System {
   function resetGame() public {
@@ -32,5 +32,6 @@ contract ResetSystem is System {
     P_GameConfig.setGameStartTimestamp(block.timestamp);
     createPlanets(); // Planet and Empire tables are reset to default values
     initPrice(); // Empire.setPointCost and OverrideCost tables are reset to default values
+    Turn.set(block.number + P_GameConfig.getTurnLengthBlocks(), EEmpire.Red, 1);
   }
 }
