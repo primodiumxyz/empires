8b8919d75232497a062b2cd1242eb2ee78b92e5c
polarzero <0xpolarzero@gmail.com>
2024-08-06 08:55:55 +0200

    fix: add back turn

--- apps/web/src/components/TimeLeft.tsx
+++ apps/web/src/components/TimeLeft.tsx
@@ -23,8 +23,7 @@ export const TimeLeft = () => {
 
   const turn = tables.Turn.use()?.value ?? 1n;
   const displayTurn = useMemo(() => {
-    // return ((turn - 1n) / 3n + 1n).toString();
-    return (turn / 3n).toString();
+    return ((turn - 1n) / 3n + 1n).toString();
   }, [turn]);
 
   return (
