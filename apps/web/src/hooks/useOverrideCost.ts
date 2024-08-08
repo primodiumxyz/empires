7460f978da86e1912b28b4bc431eb29ece451966
0xhank <zeroxhank@gmail.com>
2024-08-08 14:21:34 -0400

    feat: override cost

--- apps/web/src/hooks/useOverrideCost.ts
+++ apps/web/src/hooks/useOverrideCost.ts
@@ -6,10 +6,11 @@ import { useCore } from "@primodiumxyz/core/react";
 export const useOverrideCost = (actionType: EOverride, empireImpacted: EEmpire, actionCount: bigint) => {
   const { tables, utils } = useCore();
   const time = tables.Time.use();
+  const turn = tables.Turn.use()?.value;
 
   return useMemo(
     () => utils.getTotalCost(actionType, empireImpacted, actionCount),
-    [actionType, empireImpacted, actionCount, time],
+    [actionType, empireImpacted, actionCount, time, turn],
   );
 };
 
