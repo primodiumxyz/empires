98cada010497acc41a096a805e4f905361efc086
kethcode <kethic@kethic.com>
2024-08-09 12:24:42 -0700

    fix: remove imports

--- packages/contracts/src/systems/OverrideShieldEaterSystem.sol
+++ packages/contracts/src/systems/OverrideShieldEaterSystem.sol
@@ -2,19 +2,13 @@
 pragma solidity >=0.8.24;
 
 import { EmpiresSystem } from "systems/EmpiresSystem.sol";
-import { Turn, TacticalStrikeOverrideLog, TacticalStrikeOverrideLogData, BoostChargeOverrideLog, BoostChargeOverrideLogData, StunChargeOverrideLog, StunChargeOverrideLogData, Planet_TacticalStrikeData, Planet_TacticalStrike, P_TacticalStrikeConfig, P_OverrideConfig, Empire, CreateShipOverrideLog, CreateShipOverrideLogData, KillShipOverrideLog, KillShipOverrideLogData, ChargeShieldsOverrideLog, ChargeShieldsOverrideLogData, DrainShieldsOverrideLog, DrainShieldsOverrideLogData, Magnet, Planet, PlanetData, P_PointConfig, PlaceMagnetOverrideLog, PlaceMagnetOverrideLogData, ShieldEaterDetonateOverrideLog, ShieldEaterDetonateOverrideLogData, P_ShieldEaterConfig } from "codegen/index.sol";
-import { EEmpire, EOverride } from "codegen/common.sol";
+import { Turn, Planet, PlanetData, ShieldEaterDetonateOverrideLog, ShieldEaterDetonateOverrideLogData, P_ShieldEaterConfig } from "codegen/index.sol";
+import { EOverride } from "codegen/common.sol";
 import { LibPrice } from "libraries/LibPrice.sol";
-import { LibPoint } from "libraries/LibPoint.sol";
 import { LibOverride } from "libraries/LibOverride.sol";
-import { LibMagnet } from "libraries/LibMagnet.sol";
 import { LibShieldEater } from "libraries/LibShieldEater.sol";
 import { ShieldEater } from "codegen/index.sol";
-import { PointsMap } from "adts/PointsMap.sol";
-import { EMPIRES_NAMESPACE_ID } from "src/constants.sol";
 import { addressToId, pseudorandomEntity } from "src/utils.sol";
-import { IWorld } from "codegen/world/IWorld.sol";
-import { Balances } from "@latticexyz/world/src/codegen/index.sol";
 
 /**
  * @title OverrideSystem
