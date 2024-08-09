f0e3a741e2a98bab40b105a0d4790c187b5a2aa5
Nabs <43505437+NAB5@users.noreply.github.com>
2024-08-09 15:21:58 -0400

    fix: update player spent

--- apps/web/src/components/PriceHistory/HistoricalPointPriceModal.tsx
+++ apps/web/src/components/PriceHistory/HistoricalPointPriceModal.tsx
@@ -72,7 +72,7 @@ export const HistoricalPointPriceModal = ({}: HistoricalPointPriceModalProps) =>
 
   // Calculate KPIs
   const walletBalance = useBalance(playerAccount.address).value ?? 0n;
-  const totalInvestment = tables.Value_PlayersMap.get(playerAccount.entity)?.profit ?? 0n;
+  const totalInvestment = tables.Value_PlayersMap.get(playerAccount.entity)?.loss ?? 0n;
   const netTotalPotential = earnings - totalInvestment;
   const netTotalImmediate = earningsImmediate - totalInvestment;
 
