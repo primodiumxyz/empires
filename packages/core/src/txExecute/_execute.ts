8c91b56102a0cf57766a0300a199158785692298
polarzero <0xpolarzero@gmail.com>
2024-08-12 16:31:33 +0200

    refactor: get rid of waitForTransaction

--- packages/core/src/txExecute/_execute.ts
+++ packages/core/src/txExecute/_execute.ts
@@ -1,57 +1,19 @@
 import { CallExecutionError, ContractFunctionExecutionError, Hex, PublicClient } from "viem";
 
-import { TX_TIMEOUT } from "@core/lib";
 import { Core, TxReceipt } from "@core/lib/types";
 
 export async function _execute(
-  { network: { publicClient }, tables }: Core,
+  { network: { publicClient } }: Core,
   txPromise: () => Promise<Hex>,
   simulateTxPromise: () => Promise<void>,
 ): Promise<TxReceipt> {
-  const waitForTransaction = async (hash: Hex): Promise<TxReceipt> => {
-    let unsubscribe: (() => void) | undefined = undefined;
-
-    // Wait for the transaction to be mined and tables to be synced at the mined block
-    const waitPromise = new Promise<TxReceipt>((resolve) => {
-      publicClient
-        .waitForTransactionReceipt({ hash })
-        .then((txReceipt) => {
-          // check if tables are synced at this block
-          const lastBlockNumberProcessed = tables.SyncStatus.get()?.lastBlockNumberProcessed;
-          if (lastBlockNumberProcessed && lastBlockNumberProcessed >= txReceipt.blockNumber) {
-            resolve({ ...txReceipt, success: txReceipt.status === "success" });
-          }
-
-          // if not, wait for sync to catch up and return the receipt when it does
-          unsubscribe = tables.SyncStatus.once({
-            filter: ({ properties: { current } }) =>
-              (current?.lastBlockNumberProcessed || BigInt(0)) >= txReceipt.blockNumber,
-            do: () => resolve({ ...txReceipt, success: txReceipt.status === "success" }),
-          });
-        })
-        .catch((err) => {
-          console.log({ err });
-          resolve({ success: false, error: err instanceof Error ? err.message : "Unknown error" });
-        });
-    });
-
-    // Prepare a timeout in case the receipt can't be found in time
-    const timeoutPromise = new Promise<TxReceipt>((resolve) =>
-      setTimeout(() => {
-        unsubscribe?.();
-        resolve({ success: false, error: "Transaction not found" });
-      }, TX_TIMEOUT),
-    );
-
-    return await Promise.race([waitPromise, timeoutPromise]);
-  };
-
   let receipt: TxReceipt = { success: false, error: "" };
 
   try {
     await simulateTxPromise();
     const txHash = await txPromise();
-    receipt = await waitForTransaction(txHash);
+    const txReceipt = await publicClient.waitForTransactionReceipt({ hash: txHash });
+    receipt = { success: txReceipt.status === "success", ...txReceipt };
     console.log("[Tx] hash: ", txHash);
 
     if (!receipt.success) {
