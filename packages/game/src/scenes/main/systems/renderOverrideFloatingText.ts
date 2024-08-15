5fde2bafaca4b7b6ce00ea6d8b08752f00f3af99
polarzero <0xpolarzero@gmail.com>
2024-08-15 17:12:01 +0200

    feat: floating text for shield changes

--- packages/game/src/scenes/main/systems/renderOverrideFloatingText.ts
+++ packages/game/src/scenes/main/systems/renderOverrideFloatingText.ts
@@ -1,5 +1,5 @@
 import { Core } from "@primodiumxyz/core";
-import { namespaceWorld } from "@primodiumxyz/reactive-tables";
+import { Entity, namespaceWorld } from "@primodiumxyz/reactive-tables";
 import { PrimodiumScene } from "@game/types";
 
 export const renderOverrideFloatingText = (scene: PrimodiumScene, core: Core) => {
@@ -9,37 +9,56 @@ export const renderOverrideFloatingText = (scene: PrimodiumScene, core: Core) =>
   } = core;
   const systemsWorld = namespaceWorld(world, "systems");
 
+  tables.CreateShipOverrideLog.watch(
+    {
+      world: systemsWorld,
+      onEnter: ({ properties: { current } }) => {
+        if (!current) return;
+
+        const planet = scene.objects.planet.get(current.planetId as Entity);
+
+        if (!planet) return;
+
+        scene.audio.play("Build", "sfx", { volume: 0.25 });
+        scene.fx.emitFloatingText({ x: planet.coord.x, y: planet.coord.y - 20 }, `+${current.overrideCount}`, {
+          icon: "Ship",
+          delay: 500,
+        });
+      },
+    },
+    { runOnInit: false },
+  );
+
+  tables.KillShipOverrideLog.watch(
+    {
+      world: systemsWorld,
+      onEnter: ({ properties: { current } }) => {
+        if (!current) return;
+
+        const planet = scene.objects.planet.get(current.planetId as Entity);
+
+        if (!planet) return;
+
+        scene.audio.play("Demolish", "sfx", { volume: 0.25 });
+        scene.fx.emitFloatingText({ x: planet.coord.x, y: planet.coord.y - 20 }, `-${current.overrideCount}`, {
+          icon: "Ship",
+          color: "#ff0000",
+        });
+      },
+    },
+    { runOnInit: false },
+  );
+
   tables.Planet.watch(
     {
       world: systemsWorld,
-      onEnter: ({ entity, properties: { current, prev } }) => {
+      onUpdate: ({ entity, properties: { current, prev } }) => {
         if (!current || !prev) return;
+        if (current.shieldCount === prev.shieldCount) return;
 
         const planet = scene.objects.planet.get(entity);
         if (!planet) return;
 
-        // ships
-        if (current.shipCount < prev.shipCount) {
-          const diff = prev.shipCount - current.shipCount;
-          scene.audio.play("Demolish", "sfx", { volume: 0.25 });
-          scene.fx.emitFloatingText(
-            { x: planet.coord.x, y: planet.coord.y - 20 },
-            current.shipCount === 0n ? `ALL SHIPS DESTROYED` : `-${diff}`,
-            {
-              icon: "Ship",
-              color: "#ff0000",
-            },
-          );
-        } else if (current.shipCount > prev.shipCount) {
-          const diff = current.shipCount - prev.shipCount;
-          scene.audio.play("Build", "sfx", { volume: 0.25 });
-          scene.fx.emitFloatingText({ x: planet.coord.x, y: planet.coord.y - 25 }, `+${diff}`, {
-            icon: "Ship",
-            delay: 500,
-          });
-        }
-
-        // shields
         if (current.shieldCount < prev.shieldCount) {
           const diff = prev.shieldCount - current.shieldCount;
           scene.audio.play("Demolish", "sfx", { volume: 0.25 });
@@ -52,7 +71,7 @@ export const renderOverrideFloatingText = (scene: PrimodiumScene, core: Core) =>
               delay: 500,
             },
           );
-        } else if (current.shieldCount > prev.shieldCount) {
+        } else {
           const diff = current.shieldCount - prev.shieldCount;
           scene.audio.play("Build", "sfx", { volume: 0.25 });
           scene.fx.emitFloatingText({ x: planet.coord.x, y: planet.coord.y - 25 }, `+${diff}`, {
