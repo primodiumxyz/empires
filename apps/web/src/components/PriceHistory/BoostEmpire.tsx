7d3d8a7a53f6578fac814cf5640464b125d40446
polarzero <0xpolarzero@gmail.com>
2024-08-15 19:45:16 +0200

    feat: boost empire in dashboard, responsive

--- apps/web/src/components/PriceHistory/BoostEmpire.tsx
+++ apps/web/src/components/PriceHistory/BoostEmpire.tsx
@@ -5,26 +5,45 @@ import { useAccountClient, useCore } from "@primodiumxyz/core/react";
 import { Badge } from "@/components/core/Badge";
 import { Button } from "@/components/core/Button";
 import { SecondaryCard } from "@/components/core/Card";
+import { Dropdown } from "@/components/core/Dropdown";
+import { IconLabel } from "@/components/core/IconLabel";
 import { NumberInput } from "@/components/core/NumberInput";
 import { PointsReceived } from "@/components/shared/PointsReceived";
 import { Price } from "@/components/shared/Price";
 import { TransactionQueueMask } from "@/components/shared/TransactionQueueMask";
 import { useContractCalls } from "@/hooks/useContractCalls";
+import { useEmpires } from "@/hooks/useEmpires";
+import { useGame } from "@/hooks/useGame";
 import { useOverrideCost } from "@/hooks/useOverrideCost";
 import { useOverridePointsReceived } from "@/hooks/useOverridePointsReceived";
+import { cn } from "@/util/client";
+import { DEFAULT_EMPIRE } from "@/util/lookups";
 
-export const BoostEmpire = ({ selectedEmpire }: { selectedEmpire: EEmpire }) => {
+export const BoostEmpire = ({
+  selectedEmpire,
+  fragment,
+  shrink,
+}: {
+  selectedEmpire?: EEmpire;
+  fragment?: boolean;
+  shrink?: boolean;
+}) => {
   const {
     playerAccount: { address, publicClient },
   } = useAccountClient();
   const calls = useContractCalls();
   const { tables } = useCore();
+  const {
+    MAIN: { sprite },
+  } = useGame();
+  const empires = useEmpires();
   const time = tables.Time.use();
 
   const [amountToBoost, setAmountToBoost] = useState("0");
+  const [empire, setEmpire] = useState<EEmpire>(selectedEmpire ?? DEFAULT_EMPIRE);
   const [playerBalance, setPlayerBalance] = useState<bigint>(0n);
-  const boostPriceWei = useOverrideCost(EOverride.AirdropGold, selectedEmpire, BigInt(amountToBoost));
-  const boostPointsReceived = useOverridePointsReceived(EOverride.AirdropGold, selectedEmpire, BigInt(amountToBoost));
+  const boostPriceWei = useOverrideCost(EOverride.AirdropGold, empire, BigInt(amountToBoost));
+  const boostPointsReceived = useOverridePointsReceived(EOverride.AirdropGold, empire, BigInt(amountToBoost));
 
   useEffect(() => {
     setAmountToBoost("0");
@@ -48,27 +67,54 @@ export const BoostEmpire = ({ selectedEmpire }: { selectedEmpire: EEmpire }) => 
   };
 
   const handleSubmit = () => {
-    calls.airdropGold(selectedEmpire, BigInt(amountToBoost), boostPriceWei, boostPointsReceived.value);
+    calls.airdropGold(empire, BigInt(amountToBoost), boostPriceWei, boostPointsReceived.value);
     setAmountToBoost("0");
   };
 
   return (
     <div className="flex w-full gap-2">
-      <SecondaryCard className="w-full flex-row items-center justify-around gap-2 bg-none">
-        <NumberInput count={amountToBoost} onChange={handleInputChange} min={0} className="w-32 place-self-center" />
+      {!selectedEmpire && (
+        <SecondaryCard className="justify-center bg-black/10">
+          <Dropdown value={empire} onChange={(value) => setEmpire(value)} className="w-32 lg:w-44">
+            {Array.from(empires.entries()).map(([key, empire]) => (
+              <Dropdown.Item key={key} value={key}>
+                <IconLabel imageUri={sprite.getSprite(empire.sprites.planet)} text={empire.name} />
+              </Dropdown.Item>
+            ))}
+          </Dropdown>
+        </SecondaryCard>
+      )}
+      <SecondaryCard className={cn("w-full flex-row items-center justify-around gap-2", fragment && "bg-none")}>
+        <NumberInput count={amountToBoost} onChange={handleInputChange} min={0} className="w-40 place-self-center" />
 
-        <div className="flex gap-2">
-          <Badge size="sm" variant="error" className="p-4 lg:badge-lg">
-            <Price wei={boostPriceWei} className="text-sm text-white lg:!text-lg" />
-          </Badge>
-          <Badge size="sm" variant="secondary" className="p-4 lg:badge-lg">
-            <PointsReceived
-              points={boostPointsReceived}
-              noCaption
-              className="text-sm text-white opacity-100 lg:!text-lg"
-            />
-          </Badge>
-        </div>
+        {!shrink && (
+          <div className="flex gap-2">
+            <Badge size="sm" variant="error" className="p-4 lg:badge-lg">
+              <Price wei={boostPriceWei} className="text-sm text-white lg:!text-lg" />
+            </Badge>
+            <Badge size="sm" variant="secondary" className="p-4 lg:badge-lg">
+              <PointsReceived
+                points={boostPointsReceived}
+                noCaption
+                className="text-sm text-white opacity-100 lg:!text-lg"
+              />
+            </Badge>
+          </div>
+        )}
+        {shrink && (
+          <div className="flex flex-col gap-1">
+            <Badge size="sm" variant="error" className="w-full p-2 lg:badge-lg">
+              <Price wei={boostPriceWei} className="text-xs text-white lg:!text-sm" />
+            </Badge>
+            <Badge size="sm" variant="secondary" className="w-full p-2 lg:badge-lg">
+              <PointsReceived
+                points={boostPointsReceived}
+                noCaption
+                className="text-xs text-white opacity-100 lg:!text-sm"
+              />
+            </Badge>
+          </div>
+        )}
 
         <TransactionQueueMask id="sell-points">
           <Button
