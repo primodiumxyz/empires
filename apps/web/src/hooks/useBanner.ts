1407df1ea1477ef770f41ea5b00900f80e961770
polarzero <0xpolarzero@gmail.com>
2024-08-28 18:39:54 +0200

    feat: banner events

--- apps/web/src/hooks/useBanner.ts
+++ apps/web/src/hooks/useBanner.ts
@@ -1,36 +1,30 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useEffect, useMemo, useState } from "react";
 
 import { InterfaceIcons } from "@primodiumxyz/assets";
-import { useWorldEvents, WorldEvent } from "@primodiumxyz/core/react";
+import { useWorldEvents, WorldEvent } from "@/hooks/useWorldEvents";
 
-const EVENT_TIMEOUT = 5000;
+// Display time when there is only one event in the queue
+const DISPLAY_DURATION = 5000;
+// Display time when there are multiple events in the queue
+const QUEUE_DISPLAY_DURATION = 2000;
+
+interface BannerEvent {
+  content: string | ReactNode;
+  iconUri: string;
+}
 
 export const useBanner = () => {
   const { onEvent } = useWorldEvents();
+  const { queueEvent, processQueue } = useMemo(createQueueManager, []);
 
   const [show, setShow] = useState(false);
   const [content, setContent] = useState<string | ReactNode>("");
   const [iconUri, setIconUri] = useState<string | null>(null);
-  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
-
-  const showEvent = (content: string, iconUri: string) => {
-    if (timeoutId) clearTimeout(timeoutId);
-
-    setShow(true);
-    setContent(content);
-    setIconUri(iconUri);
-
-    setTimeoutId(
-      setTimeout(() => {
-        setShow(false);
-      }, EVENT_TIMEOUT),
-    );
-  };
 
   const getIcon = (type: WorldEvent["type"]) => {
     switch (type) {
+      // TODO: whale icon, or something to convey a large transaction
       case "whale":
-        // TODO: whale icon, or something to convey a large transaction
         return InterfaceIcons.Objective;
       case "acidRain":
         return InterfaceIcons.AcidRain;
@@ -40,8 +34,8 @@ export const useBanner = () => {
         return InterfaceIcons.Crown;
       case "planet":
         return InterfaceIcons.Planet;
+      // TODO: opportunity icon (star, idea of making good return)
       case "opportunity":
-        // TODO: opportunity icon (star, idea of making good return)
         return InterfaceIcons.Dashboard;
       default:
         return "";
@@ -49,8 +43,59 @@ export const useBanner = () => {
   };
 
   useEffect(() => {
-    onEvent((e) => showEvent(e.content, getIcon(e.type)));
-  }, [onEvent, showEvent, getIcon]);
+    onEvent((e) => {
+      queueEvent({ content: e.content, iconUri: getIcon(e.type) });
+      processQueue(setShow, setContent, setIconUri);
+    });
+  }, [onEvent, queueEvent, processQueue]);
 
   return { content, iconUri, show };
 };
+
+function createQueueManager() {
+  const queue: BannerEvent[] = [];
+  let processing = false;
+
+  const showEvent = (
+    setShow: (show: boolean) => void,
+    setContent: (content: string | ReactNode) => void,
+    setIconUri: (iconUri: string | null) => void,
+  ) => {
+    return new Promise<void>((resolve) => {
+      const event = queue[0];
+      setShow(true);
+      setContent(event.content);
+      setIconUri(event.iconUri);
+
+      setTimeout(
+        () => {
+          setShow(false);
+          queue.shift();
+          resolve();
+        },
+        queue.length > 1 ? QUEUE_DISPLAY_DURATION : DISPLAY_DURATION,
+      );
+    });
+  };
+
+  const processQueue = async (
+    setShow: (show: boolean) => void,
+    setContent: (content: string | ReactNode) => void,
+    setIconUri: (iconUri: string | null) => void,
+  ) => {
+    if (processing) return;
+    processing = true;
+
+    while (queue.length > 0) {
+      await showEvent(setShow, setContent, setIconUri);
+    }
+
+    processing = false;
+  };
+
+  function queueEvent(event: BannerEvent) {
+    queue.push(event);
+  }
+
+  return { queueEvent, processQueue };
+}
---
+++ apps/web/src/hooks/useWorldEvents.tsx
@@ -0,0 +1,260 @@
+import { ReactNode, useCallback, useEffect, useRef } from "react";
+
+import { EEmpire } from "@primodiumxyz/contracts";
+import { entityToAddress, entityToPlanetName, WORLD_EVENTS } from "@primodiumxyz/core";
+import { useCore } from "@primodiumxyz/core/react";
+import { Entity } from "@primodiumxyz/reactive-tables";
+import { decodeEntity } from "@primodiumxyz/reactive-tables/utils";
+import { Price } from "@/components/shared/Price";
+import { Username } from "@/components/shared/Username";
+import { EmpireEnumToConfig } from "@/util/lookups";
+
+const { enabled, thresholds } = WORLD_EVENTS;
+export type WorldEvent = {
+  content: string | ReactNode;
+  type: "whale" | "acidRain" | "shieldEater" | "citadel" | "planet" | "opportunity";
+};
+
+/**
+ * Prepare world events and emit them under a single stream.
+ *
+ * @returns A stream of world events to subscribe to
+ */
+export function useWorldEvents() {
+  const { tables } = useCore();
+  const callbackRef = useRef<((event: WorldEvent) => void) | undefined>(undefined);
+
+  const emit = useCallback((event: WorldEvent) => {
+    if (callbackRef.current) callbackRef.current(event);
+  }, []);
+
+  const onEvent = useCallback((cb: (event: WorldEvent) => void) => {
+    callbackRef.current = cb;
+  }, []);
+
+  const getPlanetSpan = (planetId: Entity) => {
+    const empireId = tables.Planet.get(planetId)?.empireId;
+    if (!empireId) return <span className="text-gray-400">{entityToPlanetName(planetId)}</span>;
+    const colorClass = EmpireEnumToConfig[empireId as EEmpire].textColor;
+    return <span className={colorClass}>{entityToPlanetName(planetId)}</span>;
+  };
+
+  const getEmpireSpan = (empireId: EEmpire) => (
+    <span className={EmpireEnumToConfig[empireId].textColor}>{EmpireEnumToConfig[empireId].name} empire</span>
+  );
+
+  const getPlayerSpan = (playerId: Address) => (
+    <span className="text-yellow-400">
+      <Username address={entityToAddress(playerId)} />
+    </span>
+  );
+
+  useEffect(() => {
+    const unsubscribes = [
+      // A player making a large transaction
+      // - buying a lot of ships
+      enabled.buyShips
+        ? tables.CreateShipOverrideLog.watch(
+            {
+              onEnter: ({ properties: { current } }) => {
+                if (!current) return;
+
+                if (current.ethSpent >= thresholds.ethSpent) {
+                  emit({
+                    content: (
+                      <div>
+                        {getPlayerSpan(current.playerId)} bought <Price wei={current.ethSpent} className="font-bold" />{" "}
+                        worth of ships on {getPlanetSpan(current.planetId as Entity)}
+                      </div>
+                    ),
+                    type: "whale",
+                  });
+                }
+              },
+            },
+            { runOnInit: false },
+          )
+        : undefined,
+      // - buying a lot of shields
+      enabled.buyShields
+        ? tables.ChargeShieldsOverrideLog.watch(
+            {
+              onEnter: ({ properties: { current } }) => {
+                if (!current) return;
+
+                if (current.ethSpent >= thresholds.ethSpent) {
+                  emit({
+                    content: (
+                      <div>
+                        {getPlayerSpan(current.playerId)} bought <Price wei={current.ethSpent} className="font-bold" />{" "}
+                        worth of shields on {getPlanetSpan(current.planetId as Entity)}
+                      </div>
+                    ),
+                    type: "whale",
+                  });
+                }
+              },
+            },
+            { runOnInit: false },
+          )
+        : undefined,
+      // - buying a lot of points (airdropping gold)
+      enabled.airdropGold
+        ? tables.AirdropGoldOverrideLog.watch(
+            {
+              onEnter: ({ properties: { current } }) => {
+                if (!current) return;
+                if (current.ethSpent >= thresholds.ethSpent) {
+                  emit({
+                    content: (
+                      <div>
+                        {getPlayerSpan(current.playerId)} distributed {current.goldDistributed.toLocaleString()} gold
+                        for <Price wei={current.ethSpent} className="font-bold" /> to{" "}
+                        {getEmpireSpan(current.empireId as EEmpire)}
+                      </div>
+                    ),
+                    type: "whale",
+                  });
+                }
+              },
+            },
+            { runOnInit: false },
+          )
+        : undefined,
+      // Selling points for a large amount of ETH
+      enabled.sellPoints
+        ? tables.Value_PlayersMap.watch(
+            {
+              onChange: ({ entity, properties: { current, prev } }) => {
+                const gain = (current?.gain ?? 0n) - (prev?.gain ?? 0n);
+                if (gain >= thresholds.ethSpent) {
+                  emit({
+                    content: (
+                      <div>
+                        {getPlayerSpan(entity)} just sold <Price wei={gain} className="font-bold" /> worth of points
+                      </div>
+                    ),
+                    type: "whale",
+                  });
+                }
+              },
+            },
+            { runOnInit: false },
+          )
+        : undefined,
+      // Planets captures
+      tables.Planet.watch(
+        {
+          onChange: ({ properties: { current, prev } }) => {
+            if (current?.empireId === prev?.empireId) return;
+            const prevEmpire = prev?.empireId ? EmpireEnumToConfig[prev.empireId].name : undefined;
+            const newEmpire = EmpireEnumToConfig[current.empireId].name;
+
+            // - a citadel being captured/changing ownership
+            const isCitadel = current?.isCitadel;
+            if (enabled.citadel && isCitadel) {
+              emit({
+                content: prevEmpire ? (
+                  <div>
+                    {getEmpireSpan(current.empireId as EEmpire)} captured a citadel (
+                    {getPlanetSpan(current.planetId as Entity)}) from {getEmpireSpan(prev.empireId as EEmpire)}
+                  </div>
+                ) : (
+                  <div>
+                    {getEmpireSpan(current.empireId as EEmpire)} was first to capture the{" "}
+                    {getPlanetSpan(current.planetId as Entity)} citadel
+                  </div>
+                ),
+                type: "citadel",
+              });
+            }
+
+            // TODO: an empire just took the lead in planet count
+            // Emit only if the top planet just took the lead
+            // if (enabled.planetCountLead) {
+            //   const allPlanets = tables.Planet.getAll().map((entity) => tables.Planet.get(entity));
+            //   const planetCounts = allPlanets.reduce((acc, planet) => {
+            //     const empireId = planet?.empireId;
+            //     if (!empireId) return acc;
+            //     acc[empireId] = (acc[empireId] || 0) + 1;
+            //     return acc;
+            //   }, {} as Record<EEmpire, number>);
+          },
+        },
+        { runOnInit: false },
+      ),
+      // Points for an empire became super cheap
+      enabled.pointsCheap
+        ? tables.Empire.watch(
+            {
+              onChange: ({ entity, properties: { current, prev } }) => {
+                const config = tables.P_PointConfig.get();
+                if (!current || !prev || !config) return;
+
+                if (current.pointCost <= config.minPointCost && prev.pointCost > config.minPointCost) {
+                  const { id: empireId } = decodeEntity(tables.Empire.metadata.abiKeySchema, entity);
+
+                  emit({
+                    content: <div>The price to support {getEmpireSpan(empireId as EEmpire)} could not be lower!</div>,
+                    type: "opportunity",
+                  });
+                }
+              },
+            },
+            { runOnInit: false },
+          )
+        : undefined,
+      // An acid rain destroyed a large amount of ships
+      enabled.acidRain
+        ? tables.AcidDamageOverrideLog.watch(
+            {
+              onEnter: ({ properties: { current } }) => {
+                console.log("AcidDamageOverrideLog", current);
+                if (!current) return;
+                if (current?.shipsDestroyed >= thresholds.shipsDestroyed) {
+                  emit({
+                    content: (
+                      <div>
+                        Acid Rain destroyed {current.shipsDestroyed} ships on{" "}
+                        {getPlanetSpan(current.planetId as Entity)}
+                      </div>
+                    ),
+                    type: "acidRain",
+                  });
+                }
+              },
+            },
+            { runOnInit: false },
+          )
+        : undefined,
+      // The shield eater destroyed a large amount of shields
+      enabled.shieldEater
+        ? tables.ShieldEaterDamageOverrideLog.watch(
+            {
+              onEnter: ({ properties: { current } }) => {
+                if (!current) return;
+                if (current?.shieldsDestroyed >= thresholds.shieldsDestroyed) {
+                  emit({
+                    content: (
+                      <div>
+                        Shield Eater destroyed {current.shieldsDestroyed} shields on{" "}
+                        {getPlanetSpan(current.planetId as Entity)}
+                      </div>
+                    ),
+                    type: "shieldEater",
+                  });
+                }
+              },
+            },
+            { runOnInit: false },
+          )
+        : undefined,
+    ];
+
+    return () => {
+      unsubscribes.forEach((unsub) => unsub?.());
+    };
+  }, [tables, emit]);
+
+  return { onEvent };
+}
