db578d2fc403f4d4456f804206564f524d892084
CometShock <cometshockcrypto@gmail.com>
2024-08-28 12:11:37 -0500

    feat: increase turn length to compensate for slow tx signing pattern

--- packages/contracts/config/prototypeConfig.ts
+++ packages/contracts/config/prototypeConfig.ts
@@ -14,7 +14,7 @@ export const prototypeConfig: PrototypesConfig<(typeof worldInput)["tables"]> = 
     keys: [],
     tables: {
       P_GameConfig: {
-        turnLengthBlocks: 5n,
+        turnLengthBlocks: 8n,
         goldGenRate: 1n,
         gameOverBlock: 0n, // currently handled in PostDeploy
         gameStartTimestamp: 0n, // currently handled in PostDeploy
