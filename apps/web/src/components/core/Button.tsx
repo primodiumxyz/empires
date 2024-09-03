86f2266be1466b7987b1092b69ae8f6494ddd748
polarzero <0xpolarzero@gmail.com>
2024-09-03 14:12:38 +0200

    feat: export variants

--- apps/web/src/components/core/Button.tsx
+++ apps/web/src/components/core/Button.tsx
@@ -8,7 +8,7 @@ import { Tooltip, TooltipDirection } from "@/components/core/Tooltip";
 import { useGame } from "@/hooks/useGame";
 import { cn } from "@/util/client";
 
-const buttonVariants = cva(
+export const buttonVariants = cva(
   "btn join-item items-center justify-center whitespace-nowrap ring-offset-background focus-visible:outline-none relative",
   {
     variants: {
