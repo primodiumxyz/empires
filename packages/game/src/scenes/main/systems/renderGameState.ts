02f59a227dabfcdeb30a6740b0e5d39de932416c
polarzero <0xpolarzero@gmail.com>
2024-09-06 09:31:39 +0200

    fix(temp): initial game screen

--- packages/game/src/scenes/main/systems/renderGameState.ts
+++ packages/game/src/scenes/main/systems/renderGameState.ts
@@ -17,7 +17,11 @@ export const renderGameState = (scene: PrimodiumScene, core: Core, game: GlobalA
   });
 
   core.tables.ViewMode.watch({
-    onChange: ({ properties: { current } }) => {
+    onChange: ({ entity, properties: { current } }) => {
+      // TODO: fix this in reactive tables, this is the type of the property that is stored as well
+      // because it's a persisted table, but it should not trigger listeners
+      // padHex(toHex(`__type`))
+      if (entity === ("0x00000000000000000000000000000000000000000000000000005f5f74797065" as `0x${string}`)) return;
       if (!current) return;
       game.tables.GameState.update({ onMap: current.value === EViewMode.Map });
     },
