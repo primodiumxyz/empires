296cd3084a3b73420d4f426a93ee0feba0f1070e
polarzero <0xpolarzero@gmail.com>
2024-09-12 16:49:58 +0200

    fix: remove console.log

--- apps/keeper/bin/keeper-server.ts
+++ apps/keeper/bin/keeper-server.ts
@@ -27,7 +27,6 @@ server.get("/readyz", (req, res) => res.code(200).send());
 const keeperService = new KeeperService(env.KEEPER_PRIVATE_KEY);
 
 server.addHook("preHandler", (req, reply, done) => {
-  console.log("received request", req.headers.authorization);
   if (req.headers.authorization !== `Bearer ${env.KEEPER_BEARER_TOKEN}`) {
     reply.code(401).send({ error: "Unauthorized" });
   } else {
