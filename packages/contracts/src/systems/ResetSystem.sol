14e4232e61fa2d106b2a0f29f5c70b45a481d7d5
polarzero <0xpolarzero@gmail.com>
2024-09-16 17:56:35 +0200

    fix: restrict resetGame to admin

--- packages/contracts/src/systems/ResetSystem.sol
+++ packages/contracts/src/systems/ResetSystem.sol
@@ -2,14 +2,26 @@
 pragma solidity >=0.8.24;
 
 import { System } from "@latticexyz/world/src/System.sol";
+import { NamespaceOwner } from "@latticexyz/world/src/codegen/tables/NamespaceOwner.sol";
 import { IWorld } from "codegen/world/IWorld.sol";
 import { createPlanets } from "codegen/scripts/CreatePlanets.sol";
 import { LibShieldEater } from "libraries/LibShieldEater.sol";
 import { initPrice } from "libraries/InitPrice.sol";
-import { Turn, P_GameConfig, P_GameConfigData } from "codegen/index.sol";
-import { EEmpire } from "codegen/common.sol";
+import { EMPIRES_NAMESPACE_ID } from "src/constants.sol";
+import { Role, Turn, P_GameConfig, P_GameConfigData } from "codegen/index.sol";
+import { EEmpire, ERole } from "codegen/common.sol";
 
 contract ResetSystem is System {
+  // Modifier to restrict access to admin only
+  modifier _onlyAdmin() {
+    address sender = _msgSender();
+    require(
+      Role.get(sender) == ERole.Admin || NamespaceOwner.get(EMPIRES_NAMESPACE_ID) == sender,
+      "[EmpiresSystem] Only admin"
+    );
+    _;
+  }
+
   function resetGame() public {
     IWorld world = IWorld(_world());
     world.Empires__clearLoop();
