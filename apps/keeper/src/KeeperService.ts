4f04d739fd7e0300745de9786423a34d4cdaf12e
kethcode <kethic@kethic.com>
2024-12-09 02:41:04 -0800

    wip: keeper refactor

--- apps/keeper/src/KeeperService.ts
+++ apps/keeper/src/KeeperService.ts
@@ -54,56 +54,113 @@ export class KeeperService {
   private async run(chain: ChainConfig, worldAddress: Hex, initialBlockNumber: bigint): Promise<void> {
     const { core, deployerAccount } = await this.setupCore(chain, worldAddress, initialBlockNumber);
 
-    let updating = false;
-    let gameStarted = false;
+    enum KeeperState {
+      NotReady = 1,
+      WaitingToStart,
+      GameRunning,
+      DistributingFunds,
+      ResettingGame,
+    }
+
+    let keeperState = KeeperState.NotReady;
+    let keeperStateString = "Not Ready";
+    let TxMutex = false;
+    let resetting = false;
+
     this.unsubscribe = core.tables.BlockNumber.watch({
       onChange: async ({ properties: { current } }) => {
         const ready = core.tables.Ready.get()?.value ?? false;
         const startBlock = core.tables.P_GameConfig.get()?.gameStartBlock ?? 0n;
         const endBlock = core.tables.P_GameConfig.get()?.gameOverBlock ?? 0n;
-        const blocksLeft = endBlock - (current?.value ?? 0n);
+        const blocksLeft = endBlock - (current?.value ?? 0n) - 1n;
         const gameOver = blocksLeft <= 0n;
+        const startNextRound = (current?.value ?? 0n) + 120n;
 
-        if (!ready || gameOver) {
-          console.info("SKIPPING: not ready or game over");
-          return;
-        }
 
-        if (!gameStarted && ready && (current?.value ?? 0n) >= startBlock) {
-          console.info("*** Game starting");
-          gameStarted = true;
-        }
-
-        if (gameStarted && gameOver) {
-          console.info("*** Game ending");
-          updating = true;
-          console.info("** Distributing funds");
-          await this.distributeFunds(core, deployerAccount, () => {
-            console.log("** Funds distributed");
-            gameStarted = false;
-            console.log("** Resetting game");
-            await this.setupNextRound(core, deployerAccount, current?.value + 500 ?? 0n, () => {
-              updating = false;
-            });
-          });
-        }
-
+        console.info(`STATUS[${current?.value ?? 0n}]: ${keeperStateString}`);
         const txQueueSize = core.tables.TransactionQueue.getSize();
-        if (txQueueSize > 0 || updating) {
-          console.info("SKIPPING: transaction in progress");
+        if (txQueueSize > 0 || TxMutex) {
+          // console.info("SKIPPING: transaction in progress");
           return;
         }
 
-        const nextTurnBlock = core.tables.Turn.get()?.nextTurnBlock ?? 0n;
-        if ((current?.value ?? 0n) < nextTurnBlock) {
-          console.info(`SKIPPING: current block ${current?.value} next turn block ${nextTurnBlock}`);
-          return;
-        }
+        switch (keeperState) {
 
-        updating = true;
-        await this.updateWorld(core, deployerAccount, () => {
-          updating = false;
-        });
+          case KeeperState.NotReady:
+            if (ready) {
+              keeperState = KeeperState.WaitingToStart;
+              keeperStateString = "Waiting To Start";
+            }
+            break;
+
+          case KeeperState.WaitingToStart:
+            if (ready && (current?.value ?? 0n) >= startBlock) {
+              console.info("\n*** Game starting\n");
+              keeperState = KeeperState.GameRunning;
+              keeperStateString = "Game Running";
+            }
+            break;
+
+          case KeeperState.GameRunning:
+            if (gameOver) {
+              console.info("\n*** Game ending\n");
+              // TODO: reset loop test.  remove, and go back to distro state routing
+              keeperState = KeeperState.ResettingGame;
+              keeperStateString = "Resetting Game";
+              // keeperState = KeeperState.DistributingFunds;
+              // keeperStateString = "Distributing Funds";
+              return;
+            }
+
+            const nextTurnBlock = core.tables.Turn.get()?.nextTurnBlock ?? 0n;
+            if ((current?.value ?? 0n) < nextTurnBlock) {
+              // console.info(`SKIPPING: current block ${current?.value} next turn block ${nextTurnBlock}`);
+              return;
+            }
+
+            TxMutex = true;
+            await this.updateWorld(core, deployerAccount, () => {
+              TxMutex = false;
+            });
+            break;
+
+          case KeeperState.DistributingFunds:
+            console.info("\n** Distributing funds\n");
+            TxMutex = true;
+            await this.distributeFunds(core, deployerAccount, () => {
+              console.info("\n** Funds distributed\n");
+              keeperState = KeeperState.ResettingGame;
+              keeperStateString = "Resetting Game";
+              TxMutex = false;
+            });
+            break;
+
+          case KeeperState.ResettingGame:
+            // resetGame needs to be called once for each Empire.
+            // the ready flag is set to false on the first call,
+            // and set to true when the reset is completed.
+            if (!resetting) {
+              // log the start the process
+              console.info("\n** Resetting game\n");
+              resetting = true;
+            }
+            else {
+              // if we're done, change state and exit.s
+              if ((core.tables.Ready.get()?.value ?? false) == true) {
+                keeperState = KeeperState.WaitingToStart;
+                keeperStateString = "Waiting To Start";
+                resetting = false;
+                return;
+              }
+            }
+
+            // execute a reset step
+            TxMutex = true;
+            await this.setupNextRound(core, deployerAccount, startNextRound, () => {
+              TxMutex = false;
+            });
+            break;
+        }
       },
     });
   }
@@ -171,9 +228,10 @@ export class KeeperService {
     });
   }
 
-  private async setupNextRound(core: Core, deployerAccount: LocalAccount, nextStartBlock: BigInt, onComplete: () => void): Promise<TxReceipt> {
+  private async setupNextRound(core: Core, deployerAccount: LocalAccount, nextStartBlock: bigint, onComplete: () => void): Promise<TxReceipt> {
     return await execute({
       functionName: "Empires__resetGame",
+      // @ts-expect-error Wrong type
       args: [nextStartBlock],
       options: {
         gas: 25000000n,
