09805a5ee7f0702b89ded7db0efdc04f7b92d407
kethcode <kethic@primodium.com>
2025-01-15 15:34:24 -0800

    fix: force pot refresh on intervals

--- apps/web/src/hooks/usePot.ts
+++ apps/web/src/hooks/usePot.ts
@@ -3,6 +3,8 @@ import { resourceToHex } from "@latticexyz/common";
 import { useCore } from "@primodiumxyz/core/react";
 import useWinningEmpire from "@/hooks/useWinningEmpire";
 
+import { useEffect, useState } from "react";
+
 export const usePot = () => {
   const { tables, utils } = useCore();
   const gameHex = resourceToHex({
@@ -16,10 +18,24 @@ export const usePot = () => {
     name: "",
   });
 
-  const { gameOver } = useWinningEmpire();
-  const gameEndPot = tables.Balances.useWithKeys({ namespaceId: gameHex })?.balance ?? 0n; // to be used only when the game is over
-  const pot = gameOver ? gameEndPot : gameEndPot - utils.getTotalMaxSellValue();
-  const rake = tables.Balances.useWithKeys({ namespaceId: adminHex })?.balance ?? 0n;
+  const [pot, setPot] = useState<bigint>(0n);
+  let { gameOver } = useWinningEmpire();
+  let gameEndPot = tables.Balances.useWithKeys({ namespaceId: gameHex })?.balance ?? 0n; // to be used only when the game is over
+  let rake = tables.Balances.useWithKeys({ namespaceId: adminHex })?.balance ?? 0n;
+  const refreshMs: number = 2000
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      gameOver = useWinningEmpire().gameOver;
+      gameEndPot = tables.Balances.useWithKeys({ namespaceId: gameHex })?.balance ?? 0n;
+      if (gameEndPot != 0n) {
+        setPot(gameEndPot);
+      }
+      rake = tables.Balances.useWithKeys({ namespaceId: adminHex })?.balance ?? 0n;
+    }, refreshMs);
+
+    return () => clearInterval(interval);
+  }, [refreshMs]);
 
   return { pot, gameEndPot, rake };
 };
