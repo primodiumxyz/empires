14d19c76f0863f14c3ae27664e0926814c40964a
Emerson Hsieh <emersonhsieh@berkeley.edu>
2025-01-16 12:44:20 -0800

    feat: add terms redirect

--- apps/web/src/components/redirects/terms/index.tsx
+++ apps/web/src/components/redirects/terms/index.tsx
@@ -6,7 +6,7 @@ export default function App() {
   useEffect(() => {
     setTimeout(() => {
       window.location.href =
-        "https://primodium.notion.site/Primodium-Empires-Guide-17a33ade92d38031b15be6e28a4b595a?pvs=74";
+        "https://primodium.notion.site/Primodium-Empires-Terms-of-Service-17d33ade92d380e596d1c74ab886ce06";
     }, 10);
   }, []);
 
